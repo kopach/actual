06a136db662805e0c36bf2c7d88229d6e36bd718
Joel Jeremy Marquez <joeljeremy.marquez@gmail.com>
2025-04-16 14:17:29 -0700

    [Final PR] Move remaining loot-core/client files to desktop-client feature folders

---
+++ packages/desktop-client/src/queries/runQuery.test.ts
@@ -0,0 +1,16 @@
+import { initServer } from 'loot-core/platform/client/fetch';
+import { q } from 'loot-core/shared/query';
+
+import { runQuery } from './runQuery';
+
+describe('runQuery', () => {
+  it('runs a query', async () => {
+    initServer({
+      query: query => Promise.resolve({ data: query, dependencies: [] }),
+    });
+
+    const query = q('transactions').select('*');
+    const { data } = await runQuery(query);
+    expect(data).toEqual(query.serialize());
+  });
+});
