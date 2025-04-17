00d418e08f5e62ff1e832e6f5ed3d28272e3ca0b
Joel Jeremy Marquez <joeljeremy.marquez@gmail.com>
2025-04-17 11:57:29 -0700

    Clear server on each run

--- packages/desktop-client/src/queries/runQuery.test.ts
+++ packages/desktop-client/src/queries/runQuery.test.ts
@@ -1,9 +1,13 @@
-import { initServer } from 'loot-core/platform/client/fetch';
+import { initServer, clearServer } from 'loot-core/platform/client/fetch';
 import { q } from 'loot-core/shared/query';
 
 import { runQuery } from './runQuery';
 
 describe('runQuery', () => {
+  beforeEach(() => {
+    clearServer();
+  });
+
   it('runs a query', async () => {
     initServer({
       query: query => Promise.resolve({ data: query, dependencies: [] }),
